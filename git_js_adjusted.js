d04095cbb604b9f1899ae564de355d3a554adae3
felixdie <62572778+felixdie@users.noreply.github.com>
2024-03-16 22:41:03 +0100

    Update git_js_adjusted.js

--- git_js_adjusted.js
+++ git_js_adjusted.js
@@ -1,58 +1,57 @@
-(function () {
-  let template = document.createElement("template");
-  template.innerHTML = `
-    <style>
-        :host {
-            display: block;
-            overflow: hidden;
-            width: 100%; /* Adjust width and height as needed */
-            height: 100%;
-        }
-        body, html {
-            height: 100%;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-        }
-        body::before {
-            content: '';
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: inherit;
-            filter: blur(5px);
-            z-index: -1;
-        }
-        #car {
-            position: absolute;
-            top: 50%;
-            left: -100px;
-            z-index: 0;
-            animation: flyAcross 5s linear forwards;
+(function()  {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+        <h1>Hello World</h1>
+    `;
+
+    customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+
+
+		constructor() {
+			super(); 
+			this._shadowRoot = this.attachShadow({mode: "open"});
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this._firstConnection = false;
+		}
+
+        //Fired when the widget is added to the html DOM of the page
+        connectedCallback(){
+            this._firstConnection = true;
+            this.redraw();
         }
 
-        @keyframes flyAcross {
-            from {
-                left: -100px;
-                transform: translate(0, -50%);
-            }
-            to {
-                left: 100%;
-                transform: translate(0, -50%);
+         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        disconnectedCallback(){
+        
+        }
+
+         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+		onCustomWidgetBeforeUpdate(oChangedProperties) {
+
+		}
+
+        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+		onCustomWidgetAfterUpdate(oChangedProperties) {
+            if (this._firstConnection){
+                this.redraw();
             }
         }
-    </style>
-    <img id="car" src="https://github.com/felixdie/test/blob/main/e_tron_gt.png?raw=true">
-  `;
+        
+        //When the custom widget is removed from the canvas or the analytic application is closed
+        onCustomWidgetDestroy(){
+        }
 
-  class FlyingCarWidget extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({ mode: "open" }).appendChild(template.content.cloneNode(true));
-    }
-  }
+        
+        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+        /*
+        onCustomWidgetResize(width, height){
+            redraw()
+        }
+        */
 
-  customElements.define("com-yournamespace-flyingcar", FlyingCarWidget);
+        redraw(){
+        }
+    });
 })();
