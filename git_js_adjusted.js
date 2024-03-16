d5a6c98f32097dd8e223b9518bd30d35c17cb602
felixdie <62572778+felixdie@users.noreply.github.com>
2024-03-16 22:56:42 +0100

    Update git_js_adjusted.js

--- git_js_adjusted.js
+++ git_js_adjusted.js
@@ -1,57 +1,99 @@
-(function()  {
+(function() {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
-        <h1>Hello World</h1>
+        <style>
+            :host {
+                display: block;
+                overflow: hidden;
+            }
+            .container {
+                height: 100%;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+            }
+            .container::before {
+                content: '';
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background: inherit;
+                filter: blur(5px);
+                z-index: -1;
+            }
+            #car {
+                position: absolute;
+                top: 50%;
+                left: -100px;
+                z-index: 0;
+                animation: flyAcross 5s linear forwards;
+            }
+
+            @keyframes flyAcross {
+                from {
+                    left: -100px;
+                    transform: translate(0, -50%);
+                }
+                to {
+                    left: 100%;
+                    transform: translate(0, -50%);
+                }
+            }
+        </style>
+        <div class="container">
+            <img id="car" src="https://github.com/felixdie/test/blob/main/e_tron_gt.png?raw=true">
+        </div>
     `;
 
-    customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
-
-
-		constructor() {
-			super(); 
-			this._shadowRoot = this.attachShadow({mode: "open"});
+    customElements.define('com-sap-sample-flyingcar', class FlyingCar extends HTMLElement {
+        constructor() {
+            super(); 
+            this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
-		}
+        }
 
-        //Fired when the widget is added to the html DOM of the page
+        // Fired when the widget is added to the html DOM of the page
         connectedCallback(){
-            this._firstConnection = true;
-            this.redraw();
+            if (!this._firstConnection) {
+                this._firstConnection = true;
+                this.redraw();
+            }
         }
 
-         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+        // Fired when the widget is removed from the html DOM of the page (e.g. by hide)
         disconnectedCallback(){
-        
+            // Handle any cleanup
         }
 
-         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-		onCustomWidgetBeforeUpdate(oChangedProperties) {
+        // When the custom widget is updated, the Custom Widget SDK framework executes this function first
+        onCustomWidgetBeforeUpdate(oChangedProperties) {
+            // Handle properties changes if needed
+        }
 
-		}
-
-        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-		onCustomWidgetAfterUpdate(oChangedProperties) {
+        // When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+        onCustomWidgetAfterUpdate(oChangedProperties) {
             if (this._firstConnection){
                 this.redraw();
             }
         }
         
-        //When the custom widget is removed from the canvas or the analytic application is closed
+        // When the custom widget is removed from the canvas or the analytic application is closed
         onCustomWidgetDestroy(){
+            // Handle any cleanup
         }
 
-        
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+        // When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
         /*
         onCustomWidgetResize(width, height){
-            redraw()
+            this.redraw();
         }
         */
 
         redraw(){
+            // Redrawing or updating the widget's DOM goes here
         }
     });
 })();
