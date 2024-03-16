12be194508dad243f69951627929c5b023423a03
felixdie <62572778+felixdie@users.noreply.github.com>
2024-03-16 22:03:27 +0100

    Add files via upload

--- git_js_adjusted.js
+++ git_js_adjusted.js
@@ -1,49 +1,59 @@
-(function () {
-    let template = document.createElement("template");
-    template.innerHTML = `
-        <style>
-            body, html {
-                height: 100%;
-                margin: 0;
-                padding: 0;
-                overflow: hidden;
-            }
-            body::before {
-                content: '';
-                position: fixed;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                background: inherit;
-                filter: blur(5px);
-                z-index: -1;
-            }
-            #car {
-                position: absolute;
-                top: 50%;
-                left: -100px;
-                z-index: 0;
-                animation: flyAcross 5s linear forwards;
-            }
+(function() {
+  const template = document.createElement('template');
+  template.innerHTML = `
+    <style>
+        :host {
+            display: block;
+            height: 100%;
+            overflow: hidden;
+        }
+        :host::before {
+            content: '';
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: inherit;
+            filter: blur(5px);
+            z-index: -1;
+        }
+        #car {
+            position: absolute;
+            top: 50%;
+            left: -100px;
+            z-index: 0;
+            animation: flyAcross 5s linear forwards;
+        }
 
-            @keyframes flyAcross {
-                from {
-                    left: -100px;
-                    transform: translate(0, -50%);
-                }
-                to {
-                    left: 100%;
-                    transform: translate(0, -50%);
-                }
+        @keyframes flyAcross {
+            from {
+                left: -100px;
+                transform: translate(0, -50%);
             }
-        </style>
-       <div>
-    <body>
-        <img id="car" src="https://github.com/felixdie/test/blob/main/e_tron_gt.png?raw=true">
-    </body>
-    </html>
-    </div>
-      `;
+            to {
+                left: 100%;
+                transform: translate(0, -50%);
+            }
+        }
+    </style>
+    <img id="car" src="https://github.com/felixdie/test/blob/main/e_tron_gt.png?raw=true">
+  `;
+
+  class FlyingCarAnimation extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: 'open' }).appendChild(template.content.cloneNode(true));
     }
-)();
+
+    connectedCallback() {
+      // Additional setup can be performed here
+    }
+
+    disconnectedCallback() {
+      // Cleanup can be performed here
+    }
+  }
+
+  customElements.define('flying-car-animation', FlyingCarAnimation);
+})();
