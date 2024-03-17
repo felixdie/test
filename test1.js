c319c7b4c67168bb1680e3223cffea2c36bff55a
felixdie <62572778+felixdie@users.noreply.github.com>
2024-03-17 22:27:35 +0100

    Update test1.js

--- test1.js
+++ test1.js
@@ -13,7 +13,7 @@
             this._firstConnection = false;
             this._tagContainer;
             this._tagType = "h1";
-            this._tagText = "Hello World";
+            this._tagText = "Hello World this is FD";
 
             //Adding event handler for click events
 			this.addEventListener("click", event => {
